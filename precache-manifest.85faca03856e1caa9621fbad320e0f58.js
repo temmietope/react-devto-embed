f2cac40a566ba6f3169272c7094625540b69d219
temmietope <temmieayodele@gmail.com>
2021-01-03 15:14:19 +0100

    Updates

---
+++ precache-manifest.85faca03856e1caa9621fbad320e0f58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "548763e5514fb77d5ed839f137d145d5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "81195e7faeafa9db76b5",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "acce6de85b18d3825165",
+    "url": "./static/js/2.e1141b1e.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.e1141b1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81195e7faeafa9db76b5",
+    "url": "./static/js/main.fe748118.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./static/js/main.fe748118.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c7d4bed8a4e0d9af5e3",
+    "url": "./static/js/runtime-main.98bfaa45.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.f5ddfcc4f67fdd24395710e3d4289ebe.js"
+  "./precache-manifest.85faca03856e1caa9621fbad320e0f58.js"
 );
 
 self.addEventListener('message', (event) => {
